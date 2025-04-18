f5eac985318c1d9d86e64d35e11b8938a88a45e7
Asaki0019 <109356067+aski0006@users.noreply.github.com>
2025-04-18 15:51:15 +0800

    初始化项目。 Init Project.

---
+++ src/main/java/com/asaki0019/enterprisemanagementsb/EnterpriseManagementSbApplication.java
@@ -0,0 +1,13 @@
+package com.asaki0019.enterprisemanagementsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseManagementSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseManagementSbApplication.class, args);
+    }
+
+}
